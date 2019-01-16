ec9635ee39f50f5dbf6e6f156c246a5fa7fe6f26
Paulo Romano <pauloromanocarvalho@gmail.com>
2019-01-15 23:31:03 -0200

    Add models and database context to shell

--- aegir/core/commands/shell.py
+++ aegir/core/commands/shell.py
@@ -2,10 +2,38 @@ import click
 
 from IPython import embed
 
+from aegir.core.models import (
+    Owner,
+    PDV
+)
+from aegir.core.db import session
+
+
+def get_user_ns():
+    """Return object to be pre-imported by aegir shell."""
+    return {
+        'session': session,
+        'Owner': Owner,
+        'PDV': PDV
+    }
+
+
+def get_banners():
+    """Return banners text to be presented in ipython banner."""
+    return {
+        'banner1': '::: Aegir Shell Tool :::\n',
+        'banner2': 'Available Objects:\n'
+                   '    - session: SQLAlchemy session.\n'
+                   '\n'
+                   'Available Models:\n'
+                   '    - Owner\n'
+                   '    - PDV\n'
+    }
+
 
 @click.command(
     name='shell',
     help='Execute a python interactive shell with Aegir context.'
 )
 def shell():
-    embed()
+    embed(using=False, user_ns=get_user_ns(), **get_banners())
--- tests/core/commands/test_shell.py
+++ tests/core/commands/test_shell.py
@@ -1,6 +1,10 @@
 import click
+import pytest
 
 from aegir.core.commands import shell
+from aegir.core.commands.shell import get_user_ns, get_banners
+from aegir.core.db import session
+from aegir.core.models import Owner, PDV
 
 
 class TestShellCommand:
@@ -18,3 +22,15 @@ class TestShellCommand:
         shell_command.callback()
 
         assert mocked_embed.called
+
+    @pytest.mark.parametrize('name, expected_value', (
+        ('session', session),
+        ('Owner', Owner),
+        ('PDV', PDV),
+    ))
+    def test_must_pre_import_some_aegir_objects(self, name, expected_value):
+        assert get_user_ns()[name] == expected_value
+
+    @pytest.mark.parametrize('key', (get_user_ns().keys()))
+    def test_must_show_available_objects_in_banner2(self, key):
+        assert get_banners()['banner2'].find(key) >= 0
